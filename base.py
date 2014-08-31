0f0631306faba7b2f3803cf1ae8d0aa4b7593077
Garrett Smith <g@rre.tt>
2014-08-31 15:52:32 -0500

    Updated to draftkit

--- base.py
+++ base.py
@@ -1,5 +1,6 @@
 import csv
 import sys
+import traceback
 import urllib2
 
 from bs4 import BeautifulSoup
@@ -14,27 +15,17 @@ def run_resp(name, urls, players):
 
 def run_impl(name, urls, players, resp_handler):
     sys.stderr.write("Getting data for for %s\n" % name)
-    csv_file = '%s.csv' % name
-    sys.stderr.write("Writing %s..." % csv_file)
     out = csv.writer(sys.stdout)
     for url in urls:
         resp = urllib2.urlopen(url)
         for p in players(resp_handler(resp)):
-            name = normalize_name(p['name'])
-            team = normalize_team(p['team'])
-            position = normalize_position(p['position'])
+            name = normalize.name(p['name'], p['team'])
+            team = normalize.team(p['team'])
+            position = normalize.position(p['position'])
             rank = p['rank']
             out.writerow([name, team, position, rank])
-            sys.stderr.write('.')
-    sys.stderr.write('\n')
 
-def normalize_name(name):
-    return name.upper()
-
-def normalize_team(team):
-    team_upper = team.upper()
-    return normalize.teams.get(team_upper, team_upper)
-
-def normalize_position(pos):
-    pos_upper = pos.upper()
-    return normalize.positions.get(pos_upper, pos_upper)
+def debug(e):
+    traceback.print_exc()
+    print e
+    import pdb;pdb.set_trace()
--- normalize.py
+++ normalize.py
@@ -1,19 +1,131 @@
+import re
+
 teams = {
     'ARIZONA CARDINALS': 'AZ',
     'BALTIMORE RAVENS': 'BAL',
     'BUFFALO BILLS': 'BUF',
     'CAROLINA PANTHERS': 'CAR',
+    'CHICAGO BEARS': 'CHI',
     'CINCINNATI BENGALS': 'CIN',
+    'CLEVELAND BROWNS': 'CLE',
+    'DALLAS COWBOYS': 'DAL',
     'DENVER BRONCOS': 'DEN',
+    'DETROIT LIONS': 'DET',
+    'GREEN BAY PACKERS': 'GB',
     'HOUSTON TEXANS': 'HOU',
+    'INDIANAPOLIS COLTS': 'IND',
     'KANSAS CITY CHIEFS': 'KC',
+    'MIAMI DOLPHINS': 'MIA',
+    'MINNESOTA VIKINGS': 'MN',
     'NEW ENGLAND PATRIOTS': 'NE',
     'NEW ORLEANS SAINTS': 'NO',
+    'NEW YORK GIANTS': 'NYG',
+    'NEW YORK JETS': 'NYJ',
+    'PHILADELPHIA EAGLES': 'PHI',
+    'PITTSBURGH STEELERS': 'PIT',
     'SAN FRANCISCO 49ERS': 'SF',
     'SEATTLE SEAHAWKS': 'SEA',
     'ST. LOUIS RAMS': 'STL',
     'TAMPA BAY BUCCANEERS': 'TB',
+
+    '49ERS': 'SF',
+    'BEARS': 'CHI',
+    'BENGALS': 'CIN',
+    'BILLS': 'BUF',
+    'BRONCOS': 'DEN',
+    'BROWNS': 'CLE',
+    'BUCCANEERS': 'TB',
+    'CARDINALS': 'AZ',
+    'CHIEFS': 'KC',
+    'COLTS': 'IND',
+    'COWBOYS': 'DAL',
+    'DOLPHINS': 'MIA',
+    'EAGLES': 'PHI',
+    'GIANTS': 'NYG',
+    'JETS': 'NYJ',
+    'LIONS': 'DET',
+    'PACKERS': 'GB',
+    'PANTHERS': 'CAR',
+    'PATRIOTS': 'NE',
+    'RAMS': 'STL',
+    'RAVENS': 'BAL',
+    'SAINTS': 'NO',
+    'SEAHAWKS': 'SEA',
+    'STEELERS': 'PIT',
+    'TEXANS': 'HOU',
+    'VIKINGS': 'MN',
+    'WASHINGTON': 'WAS',
+
+    'JAX': 'JAC',
+    'WSH': 'WAS',
+}
+
+defense_positions = {
+    'D/ST': 'DST',
+    'DEFENSE': 'DST'
 }
 
 positions = {
+    'DEF': 'DST',
+    'D/ST': 'DST',
+    'DEFENSE': 'DST',
+    'PK': 'K',
 }
+
+names = {
+    'ARIZONA': 'DST',
+    'BALTIMORE': 'DST',
+    'BUFFALO': 'DST',
+    'CAROLINA': 'DST',
+    'CHICAGO': 'DST',
+    'CINCINNATI': 'DST',
+    'CLEVELAND': 'DST',
+    'DALLAS': 'DST',
+    'DENVER': 'DST',
+    'DETROIT': 'DST',
+    'GREEN BAY': 'DST',
+    'HOUSTON': 'DST',
+    'INDIANAPOLIS': 'DST',
+    'KANSAS CITY': 'DST',
+    'MIAMI': 'DST',
+    'MINNESOTA': 'DST',
+    'NEW ENGLAND': 'DST',
+    'NEW ORLEANS': 'DST',
+    'NEW YORK': 'DST',
+    'PHILADELPHIA': 'DST',
+    'PITTSBURGH': 'DST',
+    'SAN FRANCISCO': 'DST',
+    'SEATTLE': 'DST',
+    'ST. LOUIS': 'DST',
+    'TAMPA BAY': 'DST',
+
+    'STEVE SMITH SR.': 'STEVE SMITH',
+    'ODELL BECKHAM JR.': 'ODELL BECKHAM',
+    'STEVIE JOHNSON': 'STEVE JOHNSON',
+    'CECIL SHORTS III': 'CECIL SHORTS',
+    "LE'VEON BELL": 'LEVEON BELL',
+    'TY HILTON': 'T.Y. HILTON',
+}
+
+def name(name, team=None):
+    name_upper = name.upper()
+    for from_str, to_str in defense_positions.items():
+        name_upper = name_upper.replace(from_str, to_str)
+    name_upper = names.get(name_upper, name_upper)
+    return normalize_defense_name(name_upper, team)
+
+def normalize_defense_name(_name, _team):
+    if not _team:
+        return _name
+    if _name.endswith('DST'):
+        return '%s DST' % team(_team)
+    else:
+        return _name
+
+def team(team):
+    team_upper = team.upper()
+    return teams.get(team_upper, team_upper)
+
+def position(pos):
+    pos_upper = pos.upper()
+    return positions.get(pos_upper, pos_upper)
